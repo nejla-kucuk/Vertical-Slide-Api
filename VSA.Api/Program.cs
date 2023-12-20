bad8001b9981ee3b4ba711354bb524cf31dd8f8f
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-12-20 21:31:52 +0300

    [Feat]: Add Project

---
+++ VSA.Api/Program.cs
@@ -0,0 +1,33 @@
+using Carter;
+using VSA.Api.Features.Brands;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapCarter();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
