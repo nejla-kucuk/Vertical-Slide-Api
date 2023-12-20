06aef3de92b069bee4391ab705d70207e65b4f65
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-12-20 21:31:19 +0300

    [Feat]: Add DbContext

---
+++ VSA.Api/Database/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using VSA.Api.Entities;
+
+namespace VSA.Api.Database
+{
+    public class AppDbContext : DbContext
+    {
+        DbSet<Instrument> Instruments { get; set; }
+
+        DbSet<Brand> Brands { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Instrument>(builder =>
+                            builder.OwnsOne(a => a.Id, tagsBuilder => tagsBuilder.ToJson()));
+
+            modelBuilder.Entity<Brand>(builder =>
+                            builder.OwnsOne(a => a.Id, tagsBuilder => tagsBuilder.ToJson()));
+        }
+    }
+}
