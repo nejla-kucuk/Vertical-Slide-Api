ab355ef8bf9b82d48acdd46333960d2d33d32e98
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-12-24 10:59:41 +0300

    [Feat]: Add Response

---
+++ VSA.Api/Features/Brands/Models/AddBrandModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace VSA.Api.Features.Brands.Models
+{
+    public class AddBrandModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string DisplayText { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime CreatedOnUtc { get; set; }
+
+
+    }
+}
