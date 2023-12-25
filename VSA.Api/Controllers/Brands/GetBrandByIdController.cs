26373c77eb7bfc0d40dce38244d1b0998bc5cb1b
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-12-25 20:11:13 +0300

    [Feat]: Add Brand Controller

---
+++ VSA.Api/Controllers/Brands/GetBrandByIdController.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using VSA.Api.Features.Brands.Models.Brands;
+using VSA.Api.Features.Brands.Queries;
+
+namespace VSA.Api.Controllers.Brands
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetBrandByIdController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public GetBrandByIdController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        [HttpGet("{id:guid}")]
+        public ActionResult<GetBrandByIdModel> GetBrandById([FromRoute] Guid id)
+        {
+            var request = new GetBrandByIdQ { Id = id };
+            var response = _mediator.Send(request);
+            return Ok(response);
+        }
+
+
+    }
+}
