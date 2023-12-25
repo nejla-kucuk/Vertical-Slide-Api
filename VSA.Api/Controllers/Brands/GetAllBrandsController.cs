5d39f9ccce8f78fd3b018c0968d3cb70e461cce2
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-12-25 20:11:07 +0300

    [Feat]: Add Brand Controller

---
+++ VSA.Api/Controllers/Brands/GetAllBrandsController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VSA.Api.Features.Brands.Models.Brands;
+using VSA.Api.Features.Brands.Queries;
+
+namespace VSA.Api.Controllers.Brands
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetAllBrandsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public GetAllBrandsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<GetAllBrandsModel>>> GetAllBrands()
+        {
+            var query = new GetAllBrands();
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
+        }
+
+    }
+}
