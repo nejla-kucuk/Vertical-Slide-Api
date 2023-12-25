6de494d3b2c0a3f17f2a5b9450114466f368e3b3
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-12-25 20:12:40 +0300

    [Feat]: Add Brand Model

---
+++ VSA.Api/Features/Brands/Models/Brands/GetBrandByIdModel.cs
@@ -0,0 +1,17 @@
+﻿using VSA.Api.Entities;
+
+namespace VSA.Api.Features.Brands.Models.Brands
+{
+    public class GetBrandByIdModel
+    {
+        public string Name { get; set; }
+
+        public string DisplayText { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime CreatedOnUtc { get; set; }
+
+        // List<Instrument> Instruments { get; set; }
+    }
+}
