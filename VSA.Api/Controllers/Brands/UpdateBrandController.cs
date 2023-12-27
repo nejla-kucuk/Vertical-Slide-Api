7ebdd666d247b47f8f436b977adb6ddf19eee371
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-12-27 22:18:59 +0300

    [Feat]: Add Update Brand Controller

---
+++ VSA.Api/Controllers/Brands/UpdateBrandController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VSA.Api.Features.Brands.Command;
+using VSA.Api.Features.Brands.Models;
+using VSA.Api.Features.Brands.Queries;
+
+namespace VSA.Api.Controllers.Brands
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UpdateBrandController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UpdateBrandController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public ActionResult<UpdateBrandModel> GetBrandById([FromBody] UpdateBrand request)
+        {
+           
+            var response = _mediator.Send(request);
+            return Ok(response);
+        }
+    }
+}
