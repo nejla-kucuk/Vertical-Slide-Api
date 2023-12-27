5b2394658cd84a636783aff81727aa5ac1b9812f
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-12-27 22:20:35 +0300

    [Update]: Change Model

---
+++ VSA.Api/Features/Brands/Models/GetAllBrandsModel.cs
@@ -0,0 +1,15 @@
+﻿namespace VSA.Api.Features.Brands.Models
+{
+    public class GetAllBrandsModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string DisplayText { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime CreatedOnUtc { get; set; }
+    }
+}
