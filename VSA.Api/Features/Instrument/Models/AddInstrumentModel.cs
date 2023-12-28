17305b73b92138a540d991d3574a62551b8fb13f
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-12-28 17:15:34 +0300

    [Feat]: AddInstrument Model

---
+++ VSA.Api/Features/Instrument/Models/AddInstrumentModel.cs
@@ -0,0 +1,15 @@
+﻿namespace VSA.Api.Features.Instrument.Models
+{
+    public class AddInstrumentModel
+    {
+        public Guid BrandId { get; set; }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public DateTime? ProductionYear { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
