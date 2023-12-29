0c0d7c7ca8f308afb6aa6215ea02066b709ec275
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-12-29 12:52:55 +0300

    [Feat]: Add Instrument Controller

---
+++ VSA.Api/Controllers/Instrument/AddInstrumentController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VSA.Api.Features.Brands.Command;
+using VSA.Api.Features.Brands.Models;
+using VSA.Api.Features.Instrument.Command;
+using VSA.Api.Features.Instrument.Models;
+
+namespace VSA.Api.Controllers.Instrument
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddInstrumentController : ControllerBase
+    {
+
+        private readonly IMediator _mediator;
+
+        public AddInstrumentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+        [HttpPost]
+        public ActionResult<AddInstrumentModel> AddBrand([FromBody] AddInstrument request)
+        {
+
+            var response = _mediator.Send(request);
+
+            return Ok(response);
+        }
+    }
+}
