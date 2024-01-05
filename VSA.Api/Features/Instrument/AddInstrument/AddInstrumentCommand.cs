8638c6d74ea7c4fef5e532a0becaa205fc7da729
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2024-01-05 13:30:53 +0300

    [Feat]: Add Command

---
+++ VSA.Api/Features/Instrument/AddInstrument/AddInstrumentCommand.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using MediatR;
+using VSA.Api.Entities;
+using VSA.Api.Infrastructure.Database;
+using VSA.Api.Shared;
+
+namespace VSA.Api.Features.Instrument.AddInstrument
+{
+    public class AddInstrumentCommand : IRequest<AddInstrumentResponse>
+    {
+        public Guid BrandId { get; set; }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public DateTime? ProductionYear { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+
+}
