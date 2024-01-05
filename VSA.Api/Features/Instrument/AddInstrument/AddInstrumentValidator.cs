89547c745b4d1b74a4e2aeaed75af4dc237a963f
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2024-01-05 13:31:30 +0300

    [Feat]: Add Validator

---
+++ VSA.Api/Features/Instrument/AddInstrument/AddInstrumentValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace VSA.Api.Features.Instrument.AddInstrument
+{
+    public class Validator : AbstractValidator<AddInstrumentCommand>
+    {
+        public Validator()
+        {
+            RuleFor(x => x.BrandId).NotEmpty();
+            RuleFor(x => x.Model).NotEmpty();
+            RuleFor(x => x.Color).NotEmpty();
+            RuleFor(x => x.Price).NotEmpty();
+        }
+    }
+}
