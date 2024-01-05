6a27c4a20b8711b2ec3d091724cb0f2f2ebdbc28
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2024-01-05 13:31:18 +0300

    [Feat]: Add Instrument Response

---
+++ VSA.Api/Features/Instrument/AddInstrument/AddInstrumentResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace VSA.Api.Features.Instrument.AddInstrument
+{
+    public class AddInstrumentResponse
+    {
+        public Guid Id { get; set; }
+
+        public Guid BrandId { get; set; }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public DateTime? ProductionYear { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
