29eb1247746308e8feb48c0f0fdea2ea5b130d8f
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2024-01-08 23:04:27 +0300

    [Feat]: Add Command

---
+++ VSA.Api/Features/Instrument/DeleteInstrument/DeleteInstrumentCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace VSA.Api.Features.Instrument.DeleteInstrument
+{
+    public class DeleteInstrumentCommand: IRequest<DeleteInstrumentResponse>
+    {
+        public Guid Id { get; set; }
+    }
+}
