ca905556b98178a43c073bfd0c6aaf8eee694457
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2024-01-08 23:05:03 +0300

    [Feat]: Add Response

---
+++ VSA.Api/Features/Instrument/DeleteInstrument/DeleteInstrumentResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace VSA.Api.Features.Instrument.DeleteInstrument
+{
+    public class DeleteInstrumentResponse
+    {
+        public Guid BrandId { get; set; } 
+
+        public string Model { get; set; } = string.Empty;
+
+        public string Color { get; set; } = string.Empty;
+
+        public DateTime? ProductionYear { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
