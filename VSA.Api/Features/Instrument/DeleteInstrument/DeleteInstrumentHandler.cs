6568f8d875aa4d04421e9f93f3f153a6a97b7b03
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2024-01-08 23:04:52 +0300

    [Feat]: Add Handler

---
+++ VSA.Api/Features/Instrument/DeleteInstrument/DeleteInstrumentHandler.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation;
+using MediatR;
+using VSA.Api.Entities;
+using VSA.Api.Features.Brands.DeleteBrand;
+using VSA.Api.Features.Instrument.AddInstrument;
+using VSA.Api.Infrastructure.Database;
+using VSA.Api.Shared;
+
+namespace VSA.Api.Features.Instrument.DeleteInstrument
+{
+    public class DeleteInstrumentHandler : IRequestHandler< DeleteInstrumentCommand, DeleteInstrumentResponse>
+    {
+        private readonly AppDbContext _dbContext;
+
+        public DeleteInstrumentHandler(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+           
+        }
+
+        public async Task<DeleteInstrumentResponse> Handle(DeleteInstrumentCommand request, CancellationToken cancellationToken)
+        {
+           var instrument = await _dbContext.Instruments.FindAsync(request.Id);
+
+            if (instrument is  null)
+            {
+                throw new ErrorException(
+                    "DeleteInstrument.NotFoundBrandId",
+                    "Not Found Instrument ID! ");
+            }
+
+            var instrumentResponse = new DeleteInstrumentResponse
+            {
+                BrandId = instrument.BrandId,
+                Model = instrument.Model,
+                Color = instrument.Color,
+                ProductionYear = instrument.ProductionYear,
+                Price = instrument.Price
+
+            };
+
+
+            _dbContext.Instruments.Remove(instrument);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return instrumentResponse;
+        }
+    }
+}
