6c9ba3d0fe8fbd502ebfc83d3fe64dda44ee5202
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2024-01-08 23:04:41 +0300

    [Feat]: Add Endpoint

---
+++ VSA.Api/Features/Instrument/DeleteInstrument/DeleteInstrumentEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using Carter;
+using MediatR;
+using VSA.Api.Features.Instrument.AddInstrument;
+
+namespace VSA.Api.Features.Instrument.DeleteInstrument
+{
+    public class DeleteInstrumentEndpoint: ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("api/deleteinstrument", async (DeleteInstrumentCommand command, ISender sender) =>
+            {
+                var response = await sender.Send(command);
+
+                return Results.Ok(response);
+            });
+        }
+    }
+}
