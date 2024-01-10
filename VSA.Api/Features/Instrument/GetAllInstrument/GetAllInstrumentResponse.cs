05749f0ad17b681c391ff4912b012031d9c98b17
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2024-01-10 22:06:10 +0300

    [Feat]: Add Response

---
+++ VSA.Api/Features/Instrument/GetAllInstrument/GetAllInstrumentResponse.cs
@@ -0,0 +1,27 @@
+﻿using VSA.Api.Entities;
+
+namespace VSA.Api.Features.Instrument.GetAllInstrument
+{
+    public class GetAllInstrumentResponse
+    {
+        public Guid BrandId { get; set; }
+
+        public string BrandName { get; set; } = string.Empty;
+
+        public string BrandDisplayText { get; set; } = string.Empty;
+
+        public string BrandAddress { get; set; } = string.Empty;
+
+
+        public string Model { get; set; } = string.Empty;
+
+
+        public string Color { get; set; } = string.Empty;
+
+
+        public DateTime? ProductionYear { get; set; }
+
+
+        public decimal Price { get; set; }
+    }
+}
