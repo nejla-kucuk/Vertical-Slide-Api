4c8c30f66ee54e9893783996442965f24e22328f
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2024-01-10 22:05:43 +0300

    [Feat]: Add Endpoint

---
+++ VSA.Api/Features/Instrument/GetAllInstrument/GetAllInstrumentEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using Carter;
+using MediatR;
+
+namespace VSA.Api.Features.Instrument.GetAllInstrument
+{
+    public class GetAllInstrumentEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("api/GetAllInstrument", async (ISender sender) =>
+            {
+                var query = new GetAllInstrumentQuery();
+
+                var response = await sender.Send(query);
+
+                return Results.Ok(response);
+            });
+        }
+    }
+}
