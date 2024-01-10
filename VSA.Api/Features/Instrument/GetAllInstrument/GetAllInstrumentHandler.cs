1b2a49768860573fe85a96ed5b71f9ca30575ce5
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2024-01-10 22:05:52 +0300

    [Feat]: Add Handler

---
+++ VSA.Api/Features/Instrument/GetAllInstrument/GetAllInstrumentHandler.cs
@@ -0,0 +1,46 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+using VSA.Api.Entities;
+using VSA.Api.Features.Brands.GetAllBrand;
+using VSA.Api.Features.Instrument.AddInstrument;
+using VSA.Api.Infrastructure.Database;
+
+namespace VSA.Api.Features.Instrument.GetAllInstrument
+{
+    public class GetAllInstrumentHandler : IRequestHandler<GetAllInstrumentQuery, List<GetAllInstrumentResponse>>
+    {
+
+        private readonly AppDbContext _dbContext;
+
+        public GetAllInstrumentHandler(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<GetAllInstrumentResponse>> Handle(GetAllInstrumentQuery request, CancellationToken cancellationToken)
+        {
+            var instruments = await _dbContext.Instruments
+                .Include(b => b.Brand)
+                .ToListAsync(cancellationToken);
+
+            var result = instruments.Select(instrument => new GetAllInstrumentResponse
+            {
+               BrandId = instrument.Id,
+               BrandName = instrument.Brand.Name,
+               BrandDisplayText = instrument.Brand.DisplayText,
+               BrandAddress = instrument.Brand.Address,
+                Model = instrument.Model,
+               Color = instrument.Color,
+               ProductionYear = instrument.ProductionYear,
+               Price = instrument.Price
+            
+
+
+            }).ToList();
+
+            return result;
+        }
+    }
+}
