91eb0510b57917fb39b18d085f9deae485977e9f
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2024-01-11 09:53:58 +0300

    [Feat]: Add Response

---
+++ VSA.Api/Features/Instrument/UpdateInstrument/UpdateInstrumentResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace VSA.Api.Features.Instrument.UpdateInstrument
+{
+    public class UpdateInstrumentResponse
+    {
+        public Guid BrandId { get; set; } = Guid.Empty;
+
+        public string Model { get; set; } = string.Empty;
+
+        public string Color { get; set; } = string.Empty;
+
+        public DateTime? ProductionYear { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
