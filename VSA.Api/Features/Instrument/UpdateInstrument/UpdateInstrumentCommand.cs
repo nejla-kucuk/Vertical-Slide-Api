10ceffb5d38286134ebcc597250bb58308e4d452
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2024-01-11 09:53:29 +0300

    [Feat]: Add Command

---
+++ VSA.Api/Features/Instrument/UpdateInstrument/UpdateInstrumentCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace VSA.Api.Features.Instrument.UpdateInstrument
+{
+    public class UpdateInstrumentCommand : IRequest<UpdateInstrumentResponse>
+    { 
+        public Guid Id { get; set; }
+
+        public Guid BrandId { get; set; } = Guid.Empty;
+
+        public string Model { get; set; } = string.Empty;
+
+        public string Color { get; set; } = string.Empty;
+
+        public DateTime? ProductionYear { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
