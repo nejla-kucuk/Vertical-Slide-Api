c813f372c64eaa7d5bc6be52f958b903c66c7b74
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2024-01-11 09:53:39 +0300

    [Feat]: Add Endpoint

---
+++ VSA.Api/Features/Instrument/UpdateInstrument/UpdateInstrumentEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using Carter;
+using MediatR;
+using VSA.Api.Features.Brands.UpdateBrand;
+
+namespace VSA.Api.Features.Instrument.UpdateInstrument
+{
+    public class UpdateInstrumentEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("api/UpdateInstrument", async (UpdateInstrumentCommand command, ISender sender) =>
+            {
+                var response = await sender.Send(command);
+
+                return Results.Ok(response);
+            });
+        }
+    }
+}
