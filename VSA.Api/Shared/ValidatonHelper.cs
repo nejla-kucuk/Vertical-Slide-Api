42774ecfdf38c39f2ce4e0090ffaf7c7e4d0aefc
Nejla KÜÇÜK <nkucuk097@gmail.com>
2024-02-05 20:29:33 +0300

    [Feat]: Add Validation Helper Class

---
+++ VSA.Api/Shared/ValidatonHelper.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+
+
+namespace VSA.Api.Shared
+{
+    public class ValidationHelper
+    {
+        private readonly IValidator _validator;
+
+        public ValidationHelper(IValidator validator)
+        {
+            _validator = validator;
+        }
+
+        public void ValidateAndThrow<T>(T request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException(nameof(request), "Request object cannot be null.");
+            }
+
+            var validationRequest = _validator.Validate((IValidationContext)request);
+            if (!validationRequest.IsValid)
+            {
+                throw new ErrorException(
+                    $"{typeof(T).Name}.Validation",
+                    "Values are not valid.");
+            }
+        }
+    }
+}
